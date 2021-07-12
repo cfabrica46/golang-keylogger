b686f53a3bc4226ce2b48a6493379f4421d254d7
Cesar <c.caycho46@gmail.com>
2021-07-12 00:12:18 -0500

    problems to OpenFile

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
+	"syscall"
+	"unsafe"
 )
 
+type InputEvent struct {
+	Time       syscall.Timeval
+	Type, Code uint16
+	Value      int32
+}
+
 func main() {
+	log.SetFlags(log.Lshortfile)
 	keyobardPath, err := findKeyboardDevice()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(keyobardPath)
+
+	f, err := os.OpenFile(keyobardPath, os.O_RDONLY, os.ModeCharDevice)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//	syscall.Open(keyobardPath, syscall.O_RDONLY, syscall.AF_KEY)
+	defer f.Close()
+
+	buffer := make([]byte, unsafe.Sizeof(InputEvent{}))
+
+	event := &InputEvent{}
+	err = binary.Read(bytes.NewBuffer(buffer), binary.LittleEndian, event)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(event)
 }
 
 func findKeyboardDevice() (string, error) {
